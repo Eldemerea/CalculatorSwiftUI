197ee4c3d451fff2ba158f68ea62e424b6dfda27
Rostyslav Andrusevych <76594620+Eldemerea@users.noreply.github.com>
2025-04-10 19:01:54 +0300

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by mac on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
